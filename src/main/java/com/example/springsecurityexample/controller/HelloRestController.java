85a6dc9aae9797c6621c8e43b1adfec9d09f9f0f
DenisIskhakov <iskhakov.denis@mail.ru>
2022-10-03 15:08:22 +0300

    1

--- src/main/java/com/example/springsecurityexample/controller/HelloRestController.java
+++ src/main/java/com/example/springsecurityexample/controller/HelloRestController.java
@@ -2,6 +2,9 @@ package com.example.springsecurityexample.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +25,11 @@ public class HelloRestController {
     @Operation(summary = "My endpoint", security = @SecurityRequirement(name = "bearerAuth"))
     @GetMapping("admin")
     public String helloAdmin() {
-        return "Hello Admin";
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication(); // те ли за кого выдаем себя(прошли -идем дальше)
+
+        UserDetails userPrincipal = (UserDetails)authentication.getPrincipal();
+        System.out.println("User principal name =" + userPrincipal.getUsername());
+        return "Hello Admin  userPrincipal.getUsername()";
     }
 
 }
